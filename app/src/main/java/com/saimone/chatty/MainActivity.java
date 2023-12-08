2c91937b99353083fec6cbd909e20d0439411146
Saimone2 <hav1802200474@gmail.com>
2023-12-08 08:56:56 +0200

    implemented switching between frames using the bottom menu, added username search page

--- app/src/main/java/com/saimone/chatty/MainActivity.java
+++ app/src/main/java/com/saimone/chatty/MainActivity.java
@@ -1,15 +1,42 @@
 package com.saimone.chatty;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ImageButton searchButton;
+    ChatFragment chatFragment;
+    ProfileFragment profileFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        searchButton = findViewById(R.id.main_search_button);
+
+        searchButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SearchUserActivity.class)));
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.menu_chat) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, chatFragment).commit();
+            }
+            if (item.getItemId() == R.id.menu_profile) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, profileFragment).commit();
+            }
+            return true;
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
     }
-
-
 }
